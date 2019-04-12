42a57a3acc7bc59996241b632d10bd3473079e80
kta5 <kta5@ucmerced.edu>
2019-04-12 15:04:16 -0700

    Added Contact Page

--- server.py
+++ server.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template, request
 app= Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 @app.route("/process", methods=['GET', 'POST'])
 def process():
     return "Hello " + request.form['student_name'] + " and your ID number is " + request.form['id']
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000,
